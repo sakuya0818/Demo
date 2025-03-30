d39544b0721cf242395bb8108df2903bd57bf12c
靳晓丹 <1015903494@qq.com>
2025-03-30 18:34:32 +0800

    添加相机类

---
+++ OPenGLTest/src/application/camera/Camera.cpp
@@ -0,0 +1,18 @@
+#include "Camera.h"
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera()
+{
+}
+
+glm::mat4 Camera::getViewMatrix()
+{
+	glm::vec3 front = glm::cross(mUp, mRight);
+	glm::vec3 center = mPosition + front;
+	glm::mat4 viewMatrix = glm::lookAt(mPosition, center, mUp);
+
+	return viewMatrix;
+}
---
+++ OPenGLTest/src/application/camera/Camera.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../../glframework/core.h"
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+	
+	glm::mat4 getViewMatrix();
+
+public:
+	glm::vec3 mPosition = {0.0f, 0.0f, 1.0f};
+	glm::vec3 mUp = {0.0f, 1.0f, 0.0f};
+	glm::vec3 mRight = { 1.0f, 0.0f, 0.0f };
+};
