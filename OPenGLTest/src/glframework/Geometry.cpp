0e032ad440c7abf2cd3d6bae5e103a441ce9d36a
靳晓丹 <1015903494@qq.com>
2025-04-03 15:44:58 +0800

    在几何类中添加创建球体

--- OPenGLTest/src/glframework/Geometry.cpp
+++ OPenGLTest/src/glframework/Geometry.cpp
@@ -1,4 +1,5 @@
 #include "Geometry.h"
+#include <vector>
 
 Geometry::Geometry()
 {
@@ -91,8 +92,91 @@ Geometry* Geometry::createBox(float size)
     return geometry;
 }
 
-Geometry* Geometry::createSphere(float size)
+Geometry* Geometry::createSphere(float radius)
 {
 	Geometry* geometry = new Geometry();
+
+	std::vector<float> positions{};
+	std::vector<float> uvs{};
+	std::vector<int> indices{};
+
+	// 经线与纬线的数量
+	int latitude = 60;  // 纬线
+	int longitude = 60; // 经线
+
+	// 计算位置和UV
+	for (int i = 0; i <= latitude; i++)
+	{
+		for (int j = 0; j <= longitude; j++)
+		{
+			float phi = i * glm::pi<float>() / latitude;
+			float theta = j * 2 * glm::pi<float>() / longitude;
+
+			float x = radius * sin(phi) * cos(theta);
+			float z = radius * sin(phi) * sin(theta);
+			float y = radius * cos(phi);
+
+			positions.push_back(x);
+			positions.push_back(y);
+			positions.push_back(z);
+
+			float u = 1.0 - (float)j / (float)longitude;
+			float v = 1.0 - (float)i / (float)latitude;
+
+			uvs.push_back(u);
+			uvs.push_back(v);
+		}
+	}
+
+	// 计算索引
+	for (int i = 0; i < latitude; i++)
+	{
+		for (int j = 0; j < longitude; j++)
+		{
+			int p1 = i * (longitude + 1) + j;
+			int p2 = p1 + longitude + 1;
+			int p3 = p1 + 1;
+			int p4 = p2 + 1;
+
+			indices.push_back(p1);
+			indices.push_back(p2);
+			indices.push_back(p3);
+
+			indices.push_back(p3);
+			indices.push_back(p2);
+			indices.push_back(p4);
+		}
+	}
+
+	geometry->mIndicesCount = indices.size();
+
+	// 生成vbo,vao
+	glGenBuffers(1, &geometry->mPosVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, geometry->mPosVbo);
+	glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(float), positions.data(), GL_STATIC_DRAW);
+
+	glGenBuffers(1, &geometry->mUvVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, geometry->mUvVbo);
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(float), uvs.data(), GL_STATIC_DRAW);
+
+	glGenBuffers(1, &geometry->mEbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, geometry->mEbo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &geometry->mVao);
+	glBindVertexArray(geometry->mVao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, geometry->mPosVbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, geometry->mUvVbo);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, geometry->mEbo);
+
+	glBindVertexArray(0);
+
 	return geometry;
 }
